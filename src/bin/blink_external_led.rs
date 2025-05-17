1062d5d87343bc3ac49ee53b35fa7e4339e40ca9
Anatolii <chemyl.inc@gmail.com>
2025-05-17 11:22:22 +0600

    blink external led

---
+++ src/bin/blink_external_led.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use rp2040_hal as hal;
+use hal::pac;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
+
+#[unsafe(link_section = ".boot2")]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO14 as an output
+    let mut led_pin = pins.gpio14.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(100);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(100);
+    }
+}
